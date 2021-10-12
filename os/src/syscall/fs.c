5844fc736a295568b8b802006c06ec6f1e9cb27d
Li Shi <shili2048@gmail.com>
2021-10-12 20:51:40 +0800

    Fs: fix sys_write

--- os/src/syscall/fs.c
+++ os/src/syscall/fs.c
@@ -6,7 +6,9 @@
 int64_t sys_write(uint64_t fd, uint8_t *buf, uint64_t len) {
   switch (fd) {
   case FD_STDOUT:
-    printf((char *)buf);
+    for (uint64_t i = 0; i < len; i++) {
+      printf("%c", buf[i]);
+    }
     return (int64_t)len;
   default:
     panic("Unsupported fd in sys_write!\n");
