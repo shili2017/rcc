b3bb707165910a45d8c36ad70cef697354475eec
Li Shi <shili2048@gmail.com>
2021-10-24 12:31:32 +0800

    Syscall: add sys_read, etc.

--- os/include/syscall.h
+++ os/include/syscall.h
@@ -5,16 +5,22 @@
 
 #include "timer.h"
 
+#define SYSCALL_READ 63
 #define SYSCALL_WRITE 64
 #define SYSCALL_EXIT 93
 #define SYSCALL_YIELD 124
 #define SYSCALL_SET_PRIORITY 140
 #define SYSCALL_GET_TIME 169
+#define SYSCALL_GETPID 172
 #define SYSCALL_MUNMAP 215
+#define SYSCALL_FORK 220
+#define SYSCALL_EXEC 221
 #define SYSCALL_MMAP 222
+#define SYSCALL_WAITPID 260
 
 int64_t syscall(uint64_t syscall_id, uint64_t a0, uint64_t a1, uint64_t a2);
 
+int64_t sys_read(uint64_t fd, char *buf, uint64_t len);
 int64_t sys_write(uint64_t fd, char *buf, uint64_t len);
 int64_t sys_exit(int exit_code);
 int64_t sys_yield();
